15238576fc0a5f743d0f02c462c4fc7c41ddb2e0
Ayaan <onestar7984@gmail.com>
2023-12-21 23:07:16 +0900

    refactor: Insta Story Title 구현

--- lib/insta_home.dart
+++ lib/insta_home.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
 
+final profileNameList = <String>[
+  '오준석',
+  '고윤정',
+  '기안84',
+  '제니',
+  '이영지',
+  '임시완',
+  '미미',
+  '안유진',
+];
+final profileLocationList = <String>[
+  '수원',
+  '서울',
+  '서울',
+  '서울',
+  '인천',
+  '서울',
+  '서울',
+  '서울',
+];
 final profileImageUrlList = <String>[
   'https://yt3.googleusercontent.com/ytc/APkrFKaawIh1ObP-_o0eaRbq-IzxF3WBT5l5GCklLYa1=s176-c-k-c0x00ffffff-no-rj',
   'https://i.namu.wiki/i/pHv3y6FEJZUrYtkjhFDCPHl4rVhRFKNgZUCPL-LtjuLAtF4BZA2KMnVm1mJnNPhC7CKXZQLaFVhvpM8_QMAIdb-KPBvc8Ux1fRWwrgn_d4rrkQT4gBetDBelHejziYCEbRudBKbZPiHvaxTPIMfFOA.webp',
   'https://i.namu.wiki/i/B7jSpErtFd2gMq3dPrCBkd_zgxDH2v6jk_MOnyARvxXZFx9M8a0-F2FINl2FcGX9GfhFfDu5nOApE8toVnRvTQ.webp',
-  'https://mblogthumb-phinf.pstatic.net/MjAyMDAyMTNfODUg/MDAxNTgxNTYxNDQ1OTkz.OSa2LB437sDEL_JLnQN2sgDuVqZuQf9mni5S9NUvSZsg.QY7LqIEnYzOyi3hV2Wq6MUj5Ma6gaEdxC3eQEx6jHocg.PNG.lccthebox4/image.png?type=w800',
+  'https://mblogthumb-phinf.pstatic.net/MjAyMDA3MjJfMjA4/MDAxNTk1MzgxMzMyNDM3.0EazNlKOY2V_xOcrl2JocK2Wm0yzSooASVOfhY3Haxog.JD_JL2GIM14UiN5NHBw7vh99uJgBVkXphcGsnr9B_Jcg.PNG.jh520130/20200722102836.png?type=w800',
   'https://i.namu.wiki/i/bMJppiXHzPRDmIcSAvJ2-QWPNmi2eJZUK6VniofpEbDTU1d6TKO59qfnwU9fGpmTCHmnhK0-WFVj1KIBzN0GJQ.webp',
   'https://entertainimg.kbsmedia.co.kr/cms/uploads/PERSON_20230925093819_a32bb381362f95c682107821e93d877b.jpg',
   'https://talkimg.imbc.com/TVianUpload/tvian/TViews/image/2023/04/27/86ae2445-a8a4-43de-8185-234c1427b977.jpg',
@@ -103,12 +123,71 @@ class _InstaHomeState extends State<InstaHome> {
                           ),
                         )
                       : Container(),
-
                 ],
               ),
             ),
           ),
-          const Expanded(flex: 9, child: Text('악')),
+          Expanded(
+            flex: 9,
+            child: ListView.builder(
+              scrollDirection: Axis.vertical,
+              itemCount: profileImageUrlList.length,
+              itemBuilder: (context, index) => Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(16.0, 16.0, 8.0, 16.0),
+                    child: Row(
+                      children: [
+                        Container(
+                          height: 40.0,
+                          width: 40.0,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: NetworkImage(profileImageUrlList[index]),
+                            ),
+                          ),
+                          margin: EdgeInsets.only(right: 8),
+                        ),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                profileNameList[index],
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15,
+                                ),
+                              ),
+                              Text(
+                                profileLocationList[index],
+                                style: TextStyle(
+                                  fontSize: 12,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: null,
+                          icon: Image.asset(
+                            'assets/images/more.png',
+                            fit: BoxFit.cover,
+                            width: MediaQuery.of(context).size.width / 20,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
       bottomNavigationBar: BottomAppBar(
